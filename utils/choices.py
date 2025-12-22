a8917febed6c55021a7621c0d6c832c3966a5b56
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-12-23 07:16:37 +0900

    🔧 프로젝트 환경 설정 (#3)
    * 🙈 .gitignore 추가 (#1)
    
    * 🎉 Django 프로젝트 시작 (#1)
    
    * 🧑‍💻 환경변수 목록 추가 (#1)
    
    - 이 환경변수 목록을 보고, 개발 시 어떤 폴더에 어떤 파일을 만들고 어떤 내용을 적어야 하는지 알 수 있습니다.
    
    * 🔥 기본 주석 삭제 (#1)
    
    * 🎨 코드 포맷팅 (#1)
    
    * 🔧 개발환경/배포환경 설정 분리 (#1)
    
    * 🎨 코드 포맷팅 (#1)
    
    * 🔧 AUTH_USER_MODEL 설정 (#1)
    
    * 🔧 언어, 시간대, 날짜·숫자 형식 설정 (#1)
    
    * 🔧 media 설정 (#1)
    
    * 🔧 CORS 설정 (#1)
    
    * 🔧 DRF 설정 (#1)
    
    * 🔧 JWT 설정 (#1)
    
    * 🎨 코드 포맷팅 (#1)
    
    * 🔧 의존성 목록 추가 (#1)
    
    * 🔨 utils 파일 추가 (#1)
    
    * 🔧 configs/urls.py에서 include 불러오기 (#1)

---
+++ utils/choices.py
@@ -0,0 +1,9 @@
+from django.db.models import TextChoices, IntegerChoices
+
+class ExampleChoices(IntegerChoices):
+    """
+    예시 코드입니다.
+    """
+    ONE   = 1, "일"
+    TWO   = 2, "이"
+    THREE = 3, "삼"
