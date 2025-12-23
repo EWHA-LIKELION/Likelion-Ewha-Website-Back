a673c30fa3e1d9620fad63942f5e438fc7894524
0euun <jeyejong70@naver.com>
2025-12-23 17:32:59 +0900

    🔨 PartChoices, MethodChoices 추가 (#5)

--- utils/choices.py
+++ utils/choices.py
@@ -7,3 +7,12 @@ class ExampleChoices(IntegerChoices):
     ONE   = 1, "일"
     TWO   = 2, "이"
     THREE = 3, "삼"
+
+class PartChoices(TextChoices):
+    PM_DESIGN = 'PM_DESIGN', '기획·디자인'
+    FRONTEND  = 'FRONTEND', '프론트엔드'
+    BACKEND   = 'BACKEND', '백엔드'
+
+class MethodChoices(TextChoices):
+    OFFLINE = 'OFFLINE', '대면'
+    ONLINE = 'ONLINE', '비대면'
