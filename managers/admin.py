ce7a37f3240f322c44fe1034a2c8706ef75f5c5b
0euun <jeyejong70@naver.com>
2025-12-23 17:35:28 +0900

    🔨 지원일정 ApplicationPeriod 모델, 면접일정 InterviewPeriod 모델 추가 (#5)

--- managers/admin.py
+++ managers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ApplicationPeriod, InterviewPeriod
 
-# Register your models here.
+admin.site.register(ApplicationPeriod)
+admin.site.register(InterviewPeriod)
--- managers/models.py
+++ managers/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from utils.choices import PartChoices, MethodChoices
 
-# Create your models here.
+class ApplicationPeriod(models.Model):
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return (
+            f"지원 기간 | "
+            f"{self.start_datetime.strftime('%Y-%m-%d %H:%M')} ~ "
+            f"{self.end_datetime.strftime('%Y-%m-%d %H:%M')}"
+        )
+
+class InterviewPeriod(models.Model):
+    part = models.CharField(
+        max_length=20,
+        choices=PartChoices.choices
+    )
+    method = models.CharField(
+        max_length=20,
+        choices=MethodChoices.choices
+    )
+    time = models.DateTimeField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return (
+            f"{self.get_part_display()} | "
+            f"{self.get_method_display()} | "
+            f"{self.time.strftime('%Y-%m-%d %H:%M')}"
+        )
