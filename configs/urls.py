06d5e0c2f6091bc09b4e4fa78bf285e4c4649b6a
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-12-29 22:29:19 +0900

    🔨 accounts 앱 추가 (#22)

--- configs/urls.py
+++ configs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('applications/', include('applications.urls')),
     path('managers/', include('managers.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
