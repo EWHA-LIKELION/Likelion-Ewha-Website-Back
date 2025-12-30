87f78d018b640a429027a9c5d4040027fcf0dc98
gyk <ganada1375@gmail.com>
2025-12-30 17:10:03 +0900

    🔨 User 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        null=False
+    ) 
+    password = models.CharField(max_length=128)
+    username = models.CharField(max_length=150, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    def __str__ (self):
+        return self.email
