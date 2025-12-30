c30830311f99027756e85d5de96d6ff12e2d0fda
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-12-30 22:38:54 +0900

    🔨 recruitments 앱 Application 뷰 추가 (#21)

--- recruitments/views.py
+++ recruitments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class Application(APIView):
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [AllowAny()]
+        else:
+            return [IsAuthenticated()]
+
+    def post(self, request:HttpRequest, format=None):
+        pass
