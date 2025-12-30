9294c36091daf3f49da7808e16ce5d64a40313d1
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-12-30 15:49:56 +0900

    🚚 RecruitmentSchedule, InterviewSchedule 모델 관련 코드를 recruitments 앱으로 이동 (#22)

--- recruitments/admin.py
+++ recruitments/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Application
+from .models import RecruitmentSchedule, InterviewSchedule, Application
 
+admin.site.register(RecruitmentSchedule)
+admin.site.register(InterviewSchedule)
 admin.site.register(Application)
