faffdc3cecf3edbb01289ac94d90b3e3d37e661d
jasmin0202 <ganada1375@gmail.com>
2025-12-30 17:58:56 +0900

    Refactor User model to remove first and last name

--- accounts/models.py
+++ accounts/models.py
@@ -2,15 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    first_name = None
+    last_name = None
     email = models.EmailField(
         unique=True,
-        blank=False,
-        null=False
-    ) 
-    password = models.CharField(max_length=128)
-    username = models.CharField(max_length=150, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    
-    def __str__ (self):
+        error_messages={'unique': "A user with that email already exists."}
+    )
+    username = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
         return self.email
