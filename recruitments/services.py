a2b36a8186e9a482531637ae5c396b1e06955613
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-12-30 23:18:29 +0900

    💡 ApplicationService post 메서드 주석 추가 (#21)

--- recruitments/services.py
+++ recruitments/services.py
@@ -6,3 +6,8 @@ class ApplicationService:
 
     def post(self):
         pass
+        # 첨부파일 S3 업로드 및 URL 생성
+        # 지원 코드 생성
+        # 지원 코드 단방향 암호화
+        # 나머지 정보 양방향 암호화
+        # 시리얼라이저 통해 요청값 검증 및 데이터베이스 저장
