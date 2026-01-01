1c970d71d45d296f918c8f23e6db83ae72a18a37
zziminally <zziminally@gmail.com>
2026-01-02 00:17:51 +0900

    🔨 지원자 수정/삭제 url path 추가

--- recruitments/urls.py
+++ recruitments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import ApplicationDetailView
 
 app_name = 'recruitments'
 
 urlpatterns = [
+    path('application/<str:student_number>/', ApplicationDetailView.as_view()),
 ]
