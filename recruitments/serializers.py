1d947ed677e350fb3b276b51d3d55be3c3629f64
zziminally <zziminally@gmail.com>
2026-01-02 00:16:25 +0900

    🔨 지원자 면접방식, 결과 수정 시리얼라이저 (#35)

--- recruitments/serializers.py
+++ recruitments/serializers.py
@@ -1,1 +1,18 @@
 from rest_framework import serializers
+from .models import Application
+from utils.choices import InterviewMethodChoices, StatusChoices
+    
+class ApplicationUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ("interview_method", "status")
+
+    def validate_interview_method(self, value):
+        if value not in InterviewMethodChoices.values:
+            raise serializers.VallidationError("유효하지 않은 항목")
+        return value
+    
+    def validate_status(self, value):
+        if value not in StatusChoices.values:
+            raise serializers.ValidationError("유효하지 않은 항목")
+        return value
