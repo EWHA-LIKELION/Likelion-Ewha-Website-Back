3b85ff09de3bf9929a009b77fcd74ce9a5baf4a7
zziminally <zziminally@gmail.com>
2026-01-03 01:35:25 +0900

    📝 면접 방식 수정 제외 주석 추가 (#35)

--- recruitments/serializers.py
+++ recruitments/serializers.py
@@ -6,12 +6,12 @@ class ApplicationUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
         fields = ("interview_method", "status")
-
+    #면접 방식 수정(기획에서 빠짐)
     def validate_interview_method(self, value):
         if value not in InterviewMethodChoices.values:
             raise serializers.VallidationError("유효하지 않은 항목")
         return value
-    
+    #합격 여부 상태
     def validate_status(self, value):
         if value not in StatusChoices.values:
             raise serializers.ValidationError("유효하지 않은 항목")
