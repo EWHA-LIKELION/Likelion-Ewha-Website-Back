e75f51b1557919e328cec83b6d6d41f1abb57580
zziminally <zziminally@gmail.com>
2026-01-03 19:11:37 +0900

    🔨 모집연도조회 url path 작성 (#37)

--- recruitments/urls.py
+++ recruitments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import RecruitmentYearListView
 
 app_name = 'recruitments'
 
 urlpatterns = [
+    path('application/years/', RecruitmentYearListView.as_view()),
 ]
