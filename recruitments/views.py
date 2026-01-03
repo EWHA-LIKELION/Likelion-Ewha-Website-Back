0cfa1a3ae42714730240da47550e11443d129fe7
zziminally <zziminally@gmail.com>
2026-01-03 19:11:09 +0900

    🔨 모집연도 조회 view 작성 (#37)

--- recruitments/views.py
+++ recruitments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import RecruitmentSchedule
+
+class RecruitmentYearListView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request):
+        schedules = RecruitmentSchedule.objects.order_by("-year")
+        years = [s.year for s in schedules]
+
+        return Response({"years": years})
