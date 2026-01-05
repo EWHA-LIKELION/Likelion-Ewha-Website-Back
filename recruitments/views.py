fdc05f556c3d5010bf6182d9f5368afa4e88bc1c
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-01-05 15:06:43 +0900

    🔨 서류 접수 기간 검증 로직 추가 (#21)

--- recruitments/views.py
+++ recruitments/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest
+from django.utils import timezone
 from rest_framework import status
+from rest_framework.exceptions import APIException, PermissionDenied
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import RecruitmentSchedule
 from .services import ApplicationService
 
 class ApplicationView(APIView):
@@ -13,7 +16,16 @@ class ApplicationView(APIView):
             return [IsAuthenticated()]
 
     def post(self, request:HttpRequest, format=None):
-        # 서류 접수 기간인지 확인
+        # 서류 접수 기간 검증
+        current_time = timezone.now()
+        try:
+            recruitment_schedule = RecruitmentSchedule.objects.get(year=current_time.year)
+        except:
+            raise APIException(detail="모집 일정이 준비되지 않았습니다.")
+
+        if not recruitment_schedule.application_start <= current_time <= recruitment_schedule.application_end:
+            raise PermissionDenied(detail="서류 접수 기간이 아닙니다.")
+
         # 시리얼라이저 통해 요청값 검증
 
         application_service = ApplicationService(request)
