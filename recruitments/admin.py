063bd6f3c4400be8adda3e9219b71b2484281241
zziminally <zziminally@gmail.com>
2026-01-07 22:08:27 +0900

    🗃️ 모집 연도 admin 추가 (#37)

--- recruitments/admin.py
+++ recruitments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import RecruitmentSchedule, InterviewSchedule, Application
+from .models import RecruitmentYear, RecruitmentSchedule, InterviewSchedule, Application
 
+admin.site.register(RecruitmentYear)
 admin.site.register(RecruitmentSchedule)
 admin.site.register(InterviewSchedule)
 admin.site.register(Application)
