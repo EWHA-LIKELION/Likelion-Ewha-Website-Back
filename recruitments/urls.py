bb3bc7dedb9309391defc20a96af130ce70d7e7d
Youngeun Seol <jeyejong70@naver.com>
2026-01-17 15:39:40 +0900

    ♻️ url pattern 수정 (#33)
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- recruitments/urls.py
+++ recruitments/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'recruitments'
 
 urlpatterns = [
-    path("recruitment-schedules/", CombinedScheduleView.as_view()),
+    path("recruitment-schedule/", CombinedScheduleView.as_view()),
     path("application/", ApplicationView.as_view())
 ]
